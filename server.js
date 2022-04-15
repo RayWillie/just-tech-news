1b81aa50931cef3fcd6f6ca614b73fb47550d0e1
mac Ray <rztechdude@yahoo.com>
2022-04-14 23:14:32 -0400

    user logout initiated

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -17,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
 
 // turn on routes
 app.use(routes);
